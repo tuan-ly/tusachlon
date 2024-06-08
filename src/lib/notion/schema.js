5c34acb4d4d3ee2fa135df1bdd6c8d62192a2696
tuan-ly <89176508+tuan-ly@users.noreply.github.com>
2024-06-08 17:00:10 +0700

    add pageSchema

--- src/lib/notion/schema.js
+++ src/lib/notion/schema.js
@@ -3,9 +3,10 @@ import { notion } from '$lib/notion/notionClient';
 
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
-async function getContent(id) {
-	const pageMd = await n2m.pageToMarkdown(id);
-	return n2m.toMarkdownString(pageMd).parent;
+//note that all the function pass through schema have to take argument pageId
+async function getContent(pageId) {
+	const pageMd = await n2m.pageToMarkdown(pageId);
+	return n2m.toMarkdownString(pageMd);
 }
 
 export const schema = {
@@ -15,7 +16,34 @@ export const schema = {
 		categories: 'Categories [DB]',
 		genres: 'Genres [DB]',
 		translators: 'Translators [DB]',
-		reviews: 'Reviews [DB]',
+		description: 'Main Review ID',
+		images: 'Images'
+	},
+	reviews: {
+		name: 'Name' //,		childContent: getContent
+	},
+	authors: {
+		name: 'Name'
+	},
+	categories: {
+		name: 'Name'
+	},
+	genres: {
+		name: 'Name'
+	},
+	translators: {
+		name: 'Name'
+	}
+	// Add other mappings as needed
+};
+export const pageSchema = {
+	books: {
+		name: 'Name',
+		authors: 'Authors [DB]',
+		categories: 'Categories [DB]',
+		genres: 'Genres [DB]',
+		translators: 'Translators [DB]',
+		description: 'Main Review ID',
 		images: 'Images'
 	},
 	reviews: {
