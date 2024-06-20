fd60f1f22310ea10349247baa9d826436c1f6e85
tuan-ly <89176508+tuan-ly@users.noreply.github.com>
2024-06-20 08:26:56 +0700

    fix error handleMissingId due to no id

--- src/lib/components/Nav_2.svelte
+++ src/lib/components/Nav_2.svelte
@@ -38,14 +38,14 @@
 
 					<li>
 						<a
-							href="#genres"
+							href="#"
 							class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
 							>Genres</a
 						>
 					</li>
 					<li>
 						<a
-							href="#about"
+							href="#"
 							class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
 							>About</a
 						>
